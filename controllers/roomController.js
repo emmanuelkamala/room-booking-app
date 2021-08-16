9f982e9d453206f56644b3ce8176d1cdce6772e0
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-17 02:42:50 +0300

    Create a controller for getting a single post

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -35,7 +35,31 @@ const newRoom = async (req, res) => {
   
 }
 
+// Get Room details => /api/rooms/:id
+const getSingleRoom = async (req, res) => {
+  try {
+      const room = await Room.findById(req.query.id);
+      if(!room){
+          res.status(404).json({
+          success: false,
+          error: 'Room not found with this ID'
+        })
+      }
+      res.status(200).json({
+      success: true,
+      room
+    })
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error: error.message
+    })
+  }
+  
+}
+
 export {
   allRooms,
-  newRoom
+  newRoom,
+  getSingleRoom
 }
