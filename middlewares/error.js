28c461b1900fa37fc832312a9ed29dfd7c396048
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-08-17 13:48:34 +0300

    Create error handler for resource

--- middlewares/error.js
+++ middlewares/error.js
@@ -6,6 +6,13 @@ export default (err, req, res, next) => {
   let error = {...err}
   error.message = err.message
 
+  // Wrong Mongoose Object Id
+
+  if (err.name === 'CastError'){
+    const message = `Resource not found. Invalid: ${err.path}`
+    error = new ErrorHandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     error,
